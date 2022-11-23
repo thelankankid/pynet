593a653ad363efd305b17477016b2b5cd15fa77e
Sheshan R <tech@sheshanratnam.com>
2022-11-23 00:07:14 -0500

    edited a script

--- scripts/ping_test.py
+++ scripts/ping_test.py
@@ -1,6 +1,21 @@
 import os
-import getpass from getpass
-import ConnectHandler from netmiko
+from getpass import getpass
+from netmiko import ConnectHandler
 
-def(ping_test)
 
+password = os.getenv("PYNET_PASSWORD") if os.getenv("PYNET_PASSWORD") else getpass()
+
+device = {
+    "device_type": "cisco_ios",
+    "host": "cisco4.lasthop.io",
+    "username": "pyclass",
+    "password": password
+}
+
+net_connect = ConnectHandler(**device)
+
+output = net_connect.send_command_timing(
+    "ping", strip_prompt=False, strip_command=False
+)
+
+#output += net_connect.send_command_timing("\n",
