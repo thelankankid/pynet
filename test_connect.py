05df805f71e299c3c80efdd1d3a466e8cdb9132e
Sheshan R <tech@sheshanratnam.com>
2022-11-28 22:18:44 -0500

    updated some files

--- test_connect.py
+++ test_connect.py
@@ -1,14 +1,13 @@
 from getpass import getpass
 from netmiko import ConnectHandler
 
-
-device1 = {
-    "host": 'cisco1.lasthop.io', 
-    "username": 'pyclass', 
+device = {
+    "host": 'cisco1.lasthop.io',
+    "username": 'pyclass',
     "password": getpass(),
     "device_type": 'cisco_ios',
     "session_log": 'my_sesion.txt'
 }
 
-net_connect = ConnectHandler(**device1)
+net_connect = ConnectHandler(**device)
 print(net_connect.send_command("show ip int br"))
