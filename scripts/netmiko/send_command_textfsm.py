61dc62a6748d4a0c82d9af194750960b9fd9b173
Sheshan R <tech@sheshanratnam.com>
2022-11-30 23:36:59 -0500

    updated script man

--- scripts/netmiko/send_command_textfsm.py
+++ scripts/netmiko/send_command_textfsm.py
@@ -1,2 +1,35 @@
 import os
 from netmiko import ConnectHandler
+from getpass import getpass
+from pprint import pprint
+
+password = os.getenv("PYNET_PASSWORD") if os.getenv("PYNET_PASSWORD") else getpass()
+
+device = {
+    "host":"cisco4.lasthop.io",
+    "username": "pyclass",
+    "password": password,
+    "device_type": "cisco_ios",
+}
+
+net_connect = ConnectHandler(**device)
+
+print()
+
+cmds = ["show version", "show lldp neighbors"]
+
+for cmd in cmds:
+    output=net_connect.send_command(cmd, use_textfsm=True)
+    print("#" * 80)
+    print(cmd)
+    print("#" * 80)
+    pprint(output)
+    print("#" * 80)
+    print()
+
+    if cmd == "show lldp neighbors":
+        print("LLDP Data Structure Type: {}".format(type(output)))
+        print("HPE Switch Connection port: {}.format(output[0][neigbor_interface"]))
+
+print()
+net_connect.disconnect()
