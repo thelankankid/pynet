a8fea2dcdf7e9dadc79feecc75fe70807d6e38d4
Sheshan R <tech@sheshanratnam.com>
2022-11-30 23:16:10 -0500

    edited the scripts again

--- scripts/netmiko/send_command_global_delay.py
+++ scripts/netmiko/send_command_global_delay.py
@@ -1,24 +1,23 @@
 import os
-from datetime import datetime
-from getpass import getpass
 from netmiko import ConnectHandler
+from getpass import getpass
+from datetime import datetime
 
 password = os.getenv("PYNET_PASSWORD") if os.getenv("PYNET_PASSWORD") else getpass()
 
-device = {
+nxos2 = {
     "host": "nxos2.lasthop.io",
-    "device_type": "cisco_nxos",
     "username": "pyclass",
     "password": password,
-    "global_delay_factor": 2
+    "device_type": "cisco_nxos",
+    "global_delay_factor": 2,
 }
-command = "show lldp neighbors details"
-net_connect = ConnectHandler(**device)
 
-# use global delay factor
+net_connect = ConnectHandler(**nxos2)
+
+cmd = "show lldp neighbors detail"
 start_time = datetime.now()
-output = net_connect.send_command(command)
-net_connect.disconnect()
+output = net_connect.send_command(cmd)
 end_time = datetime.now()
 print("#" * 80)
 print(output)
@@ -28,7 +27,7 @@ print()
 
 cmd = "show lldp neighbors detail"
 start_time = datetime.now()
-output = net_connect.send_command(cmd, delay_factor=8, fast_cli=False)
+output = net_connect.send_command(cmd, delay_factor=8)
 net_connect.disconnect()
 end_time = datetime.now()
 print("#" * 80)
