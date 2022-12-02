7f953f375c634d793463e4720db49e3914269c71
Sheshan R <tech@sheshanratnam.com>
2022-12-01 22:10:28 -0500

    new script and files

---
+++ scripts/netmiko/send_config_2.py
@@ -0,0 +1,41 @@
+import os
+from netmiko import ConnectHandler
+from getpass import getpass
+from pprint import pprint
+from datetime import datetime
+
+
+
+#start_time = datetime.now()
+
+def display_output(output)
+    print()
+    print("#" * 80)
+    print("CFG Change: ")
+    print(output)
+    print("#" * 80)
+    print()
+
+if __name__ == "__main__":
+
+    password = os.getenv("PYNET_PASSWORD") if os.getenv("PYNET_PASSWORD") else getpass()
+    nxos1 = {
+        "host":"nxos1.lasthop.io",
+        "username": "pyclass",
+        "password": password,
+        "device_type": "cisco_ios",
+        "fast_cli": True
+    }
+    nxos2 = {
+        "host":"nxos2.lasthop.io",
+        "username": "pyclass",
+        "password": password,
+        "device_type": "cisco_ios",
+        "fast_cli": True
+    }
+
+    for device in (nxos1, nxos2):
+        net_connect = ConnectHandler(**device)
+        output = net_connect.send_config_from_file("vlans.txt")
+        display_output(output)
+        net_connect.disconnect()
